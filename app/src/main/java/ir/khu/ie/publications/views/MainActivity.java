c0a699d9c677066651c30fe63e0c8fa066debdd3
Kaveh Shahedi <kavehshahedi@gmail.com>
2021-05-27 00:23:28 +0430

    Main activity xml

--- app/src/main/java/ir/khu/ie/publications/views/MainActivity.java
+++ app/src/main/java/ir/khu/ie/publications/views/MainActivity.java
@@ -1,10 +1,14 @@
 package ir.khu.ie.publications.views;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
 
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.viewpager.widget.ViewPager;
+
 import ir.khu.ie.publications.R;
+import ir.khu.ie.publications.adapters.ViewPagerAdapter;
+import me.ibrahimsn.lib.SmoothBottomBar;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -12,5 +16,38 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        initializeViewPage();
+    }
+
+    private void initializeViewPage() {
+        ViewPagerAdapter adapter = new ViewPagerAdapter(getSupportFragmentManager(), 0);
+        //adapter.addFragment(new ProfileFragment(MainActivity.this, remainTime, accountType));
+
+        ViewPager viewPager = findViewById(R.id.mainActivityViewPager);
+        viewPager.setAdapter(adapter);
+
+        SmoothBottomBar bottomBar = findViewById(R.id.mainActivityNavigationBar);
+        bottomBar.setOnItemSelectedListener(i -> {
+            viewPager.setCurrentItem(i);
+            return false;
+        });
+
+        viewPager.addOnPageChangeListener(new ViewPager.OnPageChangeListener() {
+            @Override
+            public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
+            }
+
+            @Override
+            public void onPageSelected(int position) {
+                bottomBar.setItemActiveIndex(position);
+            }
+
+            @Override
+            public void onPageScrollStateChanged(int state) {
+            }
+        });
+
+        //viewPager.setCurrentItem(2);
     }
 }
--- app/src/main/java/ir/khu/ie/publications/views/SplashActivity.java
+++ app/src/main/java/ir/khu/ie/publications/views/SplashActivity.java
@@ -2,6 +2,7 @@ package ir.khu.ie.publications.views;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import ir.khu.ie.publications.R;
@@ -12,5 +13,7 @@ public class SplashActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
+
+        startActivity(new Intent(SplashActivity.this, MainActivity.class));
     }
 }
